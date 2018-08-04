2358a68c892894a2e1d6d67a7c33ea08f53885f3
Alex Smith <queso@Alexs-MacBook-Air.local>
2018-08-04 18:55:00 -0400

    react and react router added

---
+++ app/javascript/react/containers/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Route, IndexRoute, Router, browserHistory } from 'react-router';
+
+export const App = (props) => {
+  return (
+    <Router history={browserHistory} >
+      
+    </Router>
+  )
+}
+
+export default App
