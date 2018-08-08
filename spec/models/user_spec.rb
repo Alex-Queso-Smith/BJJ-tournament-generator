4d2426b098444fef19b1f40caa1f95c02b7f0c8f
Alex Smith <queso@Alexs-MacBook-Air.local>
2018-08-08 13:00:58 -0400

    update capybara test for sign up form

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,9 +5,10 @@ RSpec.describe User, type: :model do
     let!(:user1) { FactoryBot.build(:user) }
     let!(:user2) { FactoryBot.build(:user, email: "") }
     let!(:user3) { FactoryBot.build(:user, password: "") }
-    let!(:user4) { FactoryBot.build(:user, first_name: "") }
-    let!(:user5) { FactoryBot.build(:user, last_name: "") }
-    let!(:user6) { FactoryBot.build(:user, belt: "") }
+    let!(:user4) { FactoryBot.build(:user, password_confirmation: "passwor") }
+    let!(:user5) { FactoryBot.build(:user, first_name: "") }
+    let!(:user6) { FactoryBot.build(:user, last_name: "") }
+    let!(:user7) { FactoryBot.build(:user, belt: "") }
 
     it "is valid with all required fields filled in" do
       expect(user1).to be_valid
@@ -21,16 +22,21 @@ RSpec.describe User, type: :model do
       expect(user3).to_not be_valid
     end
 
-    it "is invalid with first name missing" do
+    it "is invalid with password confirmation not matching" do
       expect(user4).to_not be_valid
     end
 
-    it "is invalid with last name missing" do
+    it "is invalid with first name missing" do
       expect(user5).to_not be_valid
     end
 
-    it "is invalid with belt missing " do
+    it "is invalid with last name missing" do
       expect(user6).to_not be_valid
+    end
+
+
+    it "is invalid with belt missing " do
+      expect(user7).to_not be_valid
     end
   end
 end
