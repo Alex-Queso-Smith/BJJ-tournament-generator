e487f501b3cc914864f7a6013c31b9f7cdfb3361
Alex Smith <queso@Alexs-MacBook-Air.local>
2018-08-08 09:24:12 -0400

    attempting to fix user model spec

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -2,6 +2,7 @@ FactoryBot.define do
   factory :user do
     sequence(:email) {|n| "user#{n}@example.com" }
     password 'password'
+    password_confirmation 'password'
     first_name "Alex"
     last_name "Smith"
     belt "Blue"
