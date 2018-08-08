a8575578d6381504f3a49462ac99af3ba73581ad
Alex Smith <queso@Alexs-MacBook-Air.local>
2018-08-08 08:41:30 -0400

    updated user model tests

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -3,6 +3,9 @@ FactoryBot.define do
     sequence(:email) {|n| "user#{n}@example.com" }
     password 'password'
     password_confirmation 'password'
+    first_name "Alex"
+    last_name "Smith"
+    belt "Blue"
   end
 
 end
