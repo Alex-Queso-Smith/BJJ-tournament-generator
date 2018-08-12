97f060582e02e8b5ff814e9d2e06880bfcf9b1e5
Alex Smith <queso@Alexs-MacBook-Air.local>
2018-08-12 12:51:25 -0400

    model udpates

--- app/models/bracket.rb
+++ app/models/bracket.rb
@@ -1,4 +1,5 @@
 class Bracket < ApplicationRecord
 
   belongs_to :tournament
+  has_many :rounds
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -16,4 +16,11 @@ class User < ApplicationRecord
   has_many :tourney_rosters, dependent: :destroy
   has_many :tournaments, through: :tourney_rosters
 
+  def name_with_nickname
+    if self.nickname
+      "#{self.first_name} '#{self.nickname}' #{self.last_name}"
+    else
+      "#{self.first_name} #{self.last_name}"
+    end
+  end
 end
