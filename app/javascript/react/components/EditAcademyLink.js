9e5ed1a3c63fc48e9c43511eac6c2cd8325e2ce4
Alex Smith <queso@Alexs-MacBook-Air.local>
2018-08-16 11:22:36 -0400

    updated styling

--- app/javascript/react/components/EditAcademyLink.js
+++ app/javascript/react/components/EditAcademyLink.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router';
 const EditAcademyLink = props => {
   return(
     <Link to={`/academies/${props.id}/edit`} >
-      <button className="button medium hover-button-yellow">
+      <button className="button medium hover-button-yellow edit-academy">
         Edit Academy Info
       </button>
     </Link>
